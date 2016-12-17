9e5445e79f826996af2c36b4ae1e3ffd6d7fe7a4
Cole Hafner (Personal MacBook) <colehafner@gmail.com>
2016-12-17 00:35:05 -0800

    Fixed mobile header.

--- app/routes/middleware.js
+++ app/routes/middleware.js
@@ -6,9 +6,9 @@ var _ = require('lodash');
 */
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
-		{ label: 'Home', key: 'home', href: '/' },
-		{ label: 'Blog', key: 'blog', href: '/blog' },
-		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
+		{ label: 'About', key: 'home', href: '/' },
+		{ label: 'Services', key: 'services', href: '/services' },
+		{ label: 'Praise', key: 'praise', href: '/praise' },
 		{ label: 'Contact', key: 'contact', href: '/contact' },
 	];
 	res.locals.user = req.user;
